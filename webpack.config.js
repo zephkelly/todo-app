6e6b473cdade9777fd5195604ebeabec15a11490
zephkelly <masterevank@gmail.com>
2022-12-22 23:30:39 +1100

    Configure webpack module bundler

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        //npm install --save-dev style-loader css-loader
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+}
